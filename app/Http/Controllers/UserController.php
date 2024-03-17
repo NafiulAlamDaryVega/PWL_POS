593357274b7fed3b94b2a1e07d04311e6342011a
Nafi'ul Alam Dary Vega <nafiulalam25@gmail.com>
2024-03-17 08:40:10 +0700

    JS 04_Praktikum 2.3 – Retreiving Aggregrates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,7 +34,8 @@ class UserController extends Controller
 
         // coba akses model UserModel
         // $user = UserModel::all(); // ambil semua data dari tabel m_user
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
         return view('user', ['data' => $user]);
     }
 }
