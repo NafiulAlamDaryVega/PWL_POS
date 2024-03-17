37c68e5bedf7cb6d526e9e6fc58eca4a31f2f857
Nafi'ul Alam Dary Vega <nafiulalam25@gmail.com>
2024-03-17 08:50:12 +0700

    JS 04_Praktikum 2.4 – Retreiving or Creating Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,8 +34,16 @@ class UserController extends Controller
 
         // coba akses model UserModel
         // $user = UserModel::all(); // ambil semua data dari tabel m_user
-        $user = UserModel::where('level_id', 2)->count();
-        // dd($user);
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
+        
         return view('user', ['data' => $user]);
     }
 }
