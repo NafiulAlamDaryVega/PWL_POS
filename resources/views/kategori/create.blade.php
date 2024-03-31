ced7b3c0eab30b1de9942012138f54d0c5829db3
Nafi'ul Alam Dary Vega <120392921+NafiulAlamDaryVega@users.noreply.github.com>
2024-03-31 12:14:45 +0700

    JS 05_Praktikum 2 – Integrasi dengan DataTables

---
+++ resources/views/kategori/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+{{-- Customize layout sections --}}
+@section('subtitle', 'kategori')
+@section('content_header_title', 'Kategori')
+@section('content_header_subtitle', 'Create')
+{{-- Content body: main page content --}}
+@section('content')
+    <div class="container">
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title">Buat kategori baru</h3>
+            </div>
+        </div>
+
+        <form method="post" action="../kategori">
+            @csrf <!-- Tambahkan ini untuk menambahkan token CSRF -->
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="kodeKategori">Kode Kategori</label>
+                    <input type="text" class="form-control" id="kodeKategori" name="kategori_kode" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="namaKategori">Nama Kategori</label>
+                    <input type="text" class="form-control" id="namaKategori" name="kategori_nama" placeholder="">
+                </div>
+            </div>
+
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    </div>
+@endsection
