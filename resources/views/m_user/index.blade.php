acace674fe763ccaa503d8c19ac2fba479813212
Nafi'ul Alam Dary Vega <120392921+NafiulAlamDaryVega@users.noreply.github.com>
2024-04-14 15:34:03 +0700

    JS 06_Tugas

--- resources/views/m_user/index.blade.php
+++ resources/views/m_user/index.blade.php
@@ -1,13 +1,11 @@
-@extends('m_user/template')
+@extends('layout.app')
+@section('subtitle', 'User')
+@section('content_header_title', 'CRUD User')
 @section('content')
-    <div class="row mt-5 mb-5">
+    <div class="row mb-3">
         <div class="col-lg-12 margin-tb">
-            <div class="float-left">
-                <h2>CRUD user</h2>
-            </div>
             <div class="float-right">
-                <a class="btn btn-success" href="{{ route('m_user.create') }}"> Input
-                    User</a>
+                <a class="btn btn-success" href="{{ route('m_user.create') }}"><i class="fas fa-plus-circle mr-1"></i> Input User</a>
             </div>
         </div>
     </div>
@@ -16,19 +14,19 @@
             <p>{{ $message }}</p>
         </div>
     @endif
-    <table class="table table-bordered">
+    <table class="table table-bordered table-striped">
         <tr>
-            <th width="20px" class="text-center">User id</th>
-            <th width="150px" class="text-center">Level id</th>
-            <th width="200px"class="text-center">Username</th>
-            <th width="200px"class="text-center">Nama</th>
-            <th width="150px"class="text-center">Password</th>
-            <th width="150px" class="text-center">Level Kode</th>
-            <th width="150px" class="text-center">Level Nama</th>
+            <th class="text-center">User id</th>
+            <th class="text-center">Level id</th>
+            <th class="text-center">Username</th>
+            <th class="text-center">Nama</th>
+            <th class="text-center">Password</th>
+            <th class="text-center">Level Kode</th>
+            <th class="text-center">Level Nama</th>
+            <th class="text-center">Action</th>
         </tr>
         @foreach ($useri as $m_user)
             <tr>
-
                 <td>{{ $m_user->user_id }}</td>
                 <td>{{ $m_user->level_id }}</td>
                 <td>{{ $m_user->username }}</td>
@@ -39,15 +37,14 @@
 
                 <td class="text-center" style="white-space: nowrap;">
                     <form action="{{ route('m_user.destroy', $m_user->user_id) }}" method="POST">
-                        <a class="btn btn-info btn-sm" href="{{ route('m_user.show', $m_user->user_id) }}">Show</a>
-                        <a class="btn btn-primary btn-sm" href="{{ route('m_user.edit', $m_user->user_id) }}">Edit</a>
+                        <a class="btn btn-info btn-sm" href="{{ route('m_user.show', $m_user->user_id) }}"><i class="fas fa-eye"></i> </a>
+                        <a class="btn btn-warning btn-sm" href="{{ route('m_user.edit', $m_user->user_id) }}"><i class="fas fa-edit"></i> </a>
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-danger btn-sm"
-                            onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">Delete</button>
+                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')"><i class="fas fa-trash"></i> </button>
                     </form>
                 </td>
             </tr>
         @endforeach
     </table>
-@endsection 
+@endsection
--- resources/views/m_user/show.blade.php
+++ resources/views/m_user/show.blade.php
@@ -1,4 +1,4 @@
-@extends('m_user/template')
+{{-- @extends('m_user/template')
 @section('content')
     <div class="row mt-5 mb-5">
         <div class="col-lg-12 margin-tb">
@@ -46,5 +46,49 @@
             </div>
         </div>
 
+    </div>
+@endsection --}}
+
+{{-- Tugas 2 --}}
+@extends('layout.app')
+@section('subtitle', 'User')
+@section('content_header_title', 'User Detail')
+@section('content')
+    <div class="container">
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title">User Detail</h3>
+            </div>
+
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="user_id">User ID:</label>
+                            <input type="text" id="user_id" name="user_id" class="form-control" value="{{ $useri->user_id }}" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="level_id">Level ID:</label>
+                            <input type="text" id="level_id" name="level_id" class="form-control" value="{{ $useri->level_id }}" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="username">Username:</label>
+                            <input type="text" id="username" name="username" class="form-control" value="{{ $useri->username }}" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="nama">Nama:</label>
+                            <input type="text" id="nama" name="nama" class="form-control" value="{{ $useri->nama }}" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password:</label>
+                            <input type="text" id="password" name="password" class="form-control" value="{{ $useri->password }}" readonly>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                            <a class="btn btn-secondary mr-3" href="{{ route('m_user.index') }}">
+                                Kembali</a>
+
+                        </div>
+                    </form>
+            </div>
+        </div>
     </div>
 @endsection
